933093df77c7f553dca068fd3fcd3801fed51b48
[winsam] <winsam003@gmail.com>
2024-07-08 23:21:23 +0900

    일단커밋

---
+++ withyoon/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import "../css/Header.css"
+
+
+
+
+export default function Header() {
+    return (
+        <div className="header_containor">
+            <span className="header_img">1</span>
+            <span className="header_img">2</span>
+            <span className="header_img">3</span>
+            <span className="header_img">4</span>
+            <span className="header_img">5</span>
+        </div>
+    )
+}
