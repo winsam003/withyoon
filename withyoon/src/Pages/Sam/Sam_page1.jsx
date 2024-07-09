50c7c773c7e98b8d883c619bb5de8daa7538c870
[winsam] <winsam003@gmail.com>
2024-07-09 21:37:50 +0900

    이제끝

--- withyoon/src/Pages/Sam/Sam_page1.jsx
+++ withyoon/src/Pages/Sam/Sam_page1.jsx
@@ -7,6 +7,7 @@ import { SiDuolingo } from "react-icons/si";
 import { MdSportsEsports } from "react-icons/md";
 import { SiRockstargames } from "react-icons/si";
 import { FaRedRiver } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 export default function Sam_page1() {
     return (
@@ -16,19 +17,19 @@ export default function Sam_page1() {
                     <div className='Sam_page1_TopLeftMenu'>
                         <span className='Sam_Page1_TopLeftAll'>로고</span>
                         <span className='Sam_Page1_TopLeftAll'>전적</span>
-                        <span className='Sam_Page1_TopLeftAll'><SiLeagueoflegends className='Sam_Page1_ReactIcon'/> 리그오브레전드</span>
-                        <span className='Sam_Page1_TopLeftAll'><IoDesktop className='Sam_Page1_ReactIcon'/> 데스크톱</span>
-                        <span className='Sam_Page1_TopLeftAll'><SiDuolingo className='Sam_Page1_ReactIcon'/> 듀오</span>
-                        <span className='Sam_Page1_TopLeftAll'><RiKakaoTalkFill className='Sam_Page1_ReactIcon'/> 톡피지지</span>
-                        <span className='Sam_Page1_TopLeftAll'><SiRockstargames className='Sam_Page1_ReactIcon'/> 이스포츠</span>
-                        <span className='Sam_Page1_TopLeftAll'><MdSportsEsports className='Sam_Page1_ReactIcon'/> 게임즈</span>
-                        <span className='Sam_Page1_TopLeftAll'><FaRedRiver className='Sam_Page1_ReactIcon'/> Gigs</span>
+                        <span className='Sam_Page1_TopLeftAll'><SiLeagueoflegends className='Sam_Page1_ReactIcon' /> 리그오브레전드</span>
+                        <span className='Sam_Page1_TopLeftAll'><IoDesktop className='Sam_Page1_ReactIcon' /> 데스크톱</span>
+                        <span className='Sam_Page1_TopLeftAll'><SiDuolingo className='Sam_Page1_ReactIcon' /> 듀오</span>
+                        <span className='Sam_Page1_TopLeftAll'><RiKakaoTalkFill className='Sam_Page1_ReactIcon' /> 톡피지지</span>
+                        <span className='Sam_Page1_TopLeftAll'><SiRockstargames className='Sam_Page1_ReactIcon' /> 이스포츠</span>
+                        <span className='Sam_Page1_TopLeftAll'><MdSportsEsports className='Sam_Page1_ReactIcon' /> 게임즈</span>
+                        <span className='Sam_Page1_TopLeftAll'><FaRedRiver className='Sam_Page1_ReactIcon' /> Gigs</span>
                     </div>
                     <div className='Sam_page1_TopRightMenu'>
                         <span className='Sam_page1_TopRightAll'>문의하기</span>
                         <span className='Sam_page1_TopRightAll'>어두워지기</span>
                         <span className='Sam_page1_TopRightAll'>언어</span>
-                        <span className='Sam_page1_TopRightAll'>로그인</span>
+                        <span className='Sam_page1_TopRightAll'><Link to='/'>돌아가기</Link></span>
                     </div>
                 </div>
             </div>
